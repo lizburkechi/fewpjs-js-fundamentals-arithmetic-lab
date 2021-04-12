14c14791baba96b7ec85ff58a2e66a672fd6266b
lizburkechi <liz.burke.chi@gmail.com>
2021-04-12 12:44:25 -0500

    commit tree 96d4c4da1c95c557f16aa0c204ddc6ee53df7a2e

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 let num1 = 2;
 let num2 = 31;
 
-let multiply = num1 * num2;
+const multiply = num1 * num2 
 
-let random = Math.floor(Math.random() * 323) + 1;
+const random = Math.ceil(Math.random());
 
-let num3 = 34;
-let num4 = 15;
+let num3 = 24;
+let num4 = 5;
 
-let mod = num3 % num4;
+const mod = (num3 % num4);
 
-let max = Math.max(1, 2, 3, 20);
+const max = Math.max(1, 8, 19, 20)
